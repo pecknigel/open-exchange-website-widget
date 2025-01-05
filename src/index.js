69277f1f24a532c690a95111c9fb25e26681ad3b
Rohan <rocrafter500@outlook.com>
2025-01-05 16:49:38 +0000

    refactor to class

--- src/index.js
+++ src/index.js
@@ -1,4 +1,3 @@
-// Create element instead of templates
 const mockData = {
   TSM: 97.40377661117827,
   DTO: 51.35884532641455,
@@ -6,82 +5,79 @@ const mockData = {
   JFC: 97.83627459809185
 };
 
-const widgetTemplate = document.getElementById("widget-template");
-const rowTemplate = document.getElementById("widget-row-template");
-const widgetWrapper = document.getElementById("widget-wrapper");
-const error = document.getElementById("widget-error");
 const currencyTypes = ["Pound", "Dollar", "Euro", "Yen"];
-let currentCurrency = "£"; // set default currency based off region.
+let currentCurrency = "£";
 
-configure();
+class Widget {
+  base;
+  baseElement;
+  baseLocation;
 
-function configure() {
-  renderTable()
-}
+  constructor(baseElement="div", baseLocation="body") {
+    this.baseElement = baseElement;
+    this.baseLocation = baseLocation;
 
-// Widget Functions
-function renderTable() {
-  const widgetElement = document.querySelector(".widget");
-  const templateClone = widgetTemplate.content.cloneNode(true);
-
-  // Remove the widget element.
-  if (widgetElement) widgetElement.remove();
-
-  // Add currency button event listeners
-  currencyTypes.forEach(currency => {
-    const button = templateClone.getElementById(`button${currency}`);
-    button.addEventListener("click", () => updateCurrency(currency));
-  })
-
-  // Adds the widget element.
-  widgetWrapper.appendChild(templateClone);
-
-  // Adds the table rows.
-  const table = document.querySelector(".widget-table tbody");
-  for (const [key, value] of Object.entries(mockData)) {
-    table.appendChild(addTableRow(key, value));
+    this.renderWidget();
+    this.configureButtons();
   }
 
-  // Hides the error message.
-  error.classList.add("hidden");
-
-  // Configure temporary debug tools
-  configureDebug();
-}
-
-function addTableRow(tickerId, price) {
-  const templateClone = rowTemplate.content.cloneNode(true);
-  templateClone.querySelector("th").textContent = tickerId;
-  templateClone.querySelector("td").textContent = `${currentCurrency}${price.toFixed(2)}`;
-  return templateClone;
-}
-
-function updateCurrency(newCurrency) {
-  console.log("# run updateCurrency")
-  switch (newCurrency) {
-    case "Pound":
-      currentCurrency = "£";
-      break;
-    case "Dollar":
-      currentCurrency = "$";
-      break;
-    case "Euro":
-      currentCurrency = "€";
-      break;
-    case "Yen":
-      currentCurrency = "¥";
-      break;
+  renderWidget() {
+    if (document.querySelector(".widget")) document.querySelector(".widget").remove();
+    this.createBase();
+    this.createTable();
   }
-  console.log('newCurrency = ', newCurrency);
-  console.log('currentCurrency = ', currentCurrency);
-  renderTable()
+
+  createBase() {
+    this.base = document.createElement(this.baseElement);
+    document.querySelector(this.baseLocation).appendChild(this.base);
+    this.base.classList.add("widget");
+  }
+
+  createTable() {
+    const table = document.createElement("table");
+    const tbody = document.createElement("tbody");
+    table.appendChild(tbody);
+
+    for (const [key, value] of Object.entries(mockData)) {
+      const tr = document.createElement("tr");
+      const th = document.createElement("th");
+      const td = document.createElement("td");
+      tr.appendChild(th);
+      tr.appendChild(td);
+      th.textContent = key;
+      th.scope = "row";
+      td.textContent = `${currentCurrency}${value.toFixed(2)}`;
+
+      tbody.appendChild(tr);
+    }
+
+    this.base.appendChild(table);
+  }
+
+  configureButtons() {
+    currencyTypes.forEach(currency => {
+      const button = this.base.getElementById(`button${currency}`);
+      button.addEventListener("click", () => this.updateCurrency(currency));
+    })
+  }
+
+  updateCurrency(newCurrency) {
+    switch (newCurrency) {
+      case "Pound":
+        currentCurrency = "£";
+        break;
+      case "Dollar":
+        currentCurrency = "$";
+        break;
+      case "Euro":
+        currentCurrency = "€";
+        break;
+      case "Yen":
+        currentCurrency = "¥";
+        break;
+    }
+    this.renderWidget();
+  }
 }
 
-function configureDebug() {
-  const widgetElement = document.querySelector(".widget");
-  const portraitButton = document.getElementById("portrait-button");
-  const landscapeButton = document.getElementById("landscape-button");
-
-  portraitButton.addEventListener("click", () => widgetElement.dataset.display = "portrait");
-  landscapeButton.addEventListener("click", () => widgetElement.dataset.display = "landscape");
-}
+const tickerWidget = new Widget();
